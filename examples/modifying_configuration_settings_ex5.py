d6c206cecb5efcc235bbe7decf6f84d47d0457eb
Karsten <khakes@sandiego.edu>
2025-11-06 21:40:49 -0800

    corrected all serial ports to be proper one

--- examples/modifying_configuration_settings_ex5.py
+++ examples/modifying_configuration_settings_ex5.py
@@ -38,7 +38,7 @@ import serial
 
 from ublox_gps import UbloxGps
 
-port = serial.Serial('/dev/serial0', baudrate=38400, timeout=1)
+port = serial.Serial('/dev/ttyTHS1', baudrate=38400, timeout=1)
 gps = UbloxGps(port)
 
 def run():
