efbca140794a7819a5ed88e9800c4149bc55b5c0
Fa1conX <fa1conX01@protonmail.com>
2025-11-20 14:17:43 -0800

    created test script for ... testing!

--- test_script.py
+++ test_script.py
@@ -1,31 +1,13 @@
-"""An example of how to use the light parser for parsing UBX messages from a serial device.
-
-You will need to change the port name to that of the port you want to connect to. Also make sure that the baud rate is
-correct and that the device has been setup to output the messages via UBX protocol to your desired port!
-
-The serial package could easily be replaced with an alternative.
 """
+Test script for testing ability of jetson to control vehicle movement.
+"""
+
 import serial
+import time
+import jetson_control
 
-from ublox_gps import UbloxGps
+port = serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout=1)
 
-port = serial.Serial('/dev/ttyTHS1', baudrate=38400, timeout=1)
-gps = UbloxGps(port)
-
-def run():
-
-    try:
-        print("Listening for UBX Packets")
-        while True:
-            try:
-                print(gps.ublox_debug())
-
-            except (ValueError, IOError) as err:
-                print(err)
-
-    finally:
-        port.close()
-
-
-if __name__ == '__main__':
-    run()
+jetson_control.send_command(serial_conn=port, throttle=10, steering=90)  # Example command to move forward
+time.sleep(1)
+jetson_control.send_command(serial_conn=port, throttle=-10, steering=-80)  # Stop the vehicle
