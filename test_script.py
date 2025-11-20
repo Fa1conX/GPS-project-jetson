916e6d78b912f0ab71c6c5890660748e9f9c4995
Fa1conX <fa1conX01@protonmail.com>
2025-11-20 14:39:53 -0800

    testing

--- test_script.py
+++ test_script.py
@@ -8,6 +8,6 @@ import jetson_control
 
 port = serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout=1)
 
-jetson_control.send_command(serial_conn=port, throttle=10, steering=90)  # Example command to move forward
-time.sleep(1)
-jetson_control.send_command(serial_conn=port, throttle=-10, steering=-80)  # Stop the vehicle
+jetson_control.send_command(serial_conn=port, throttle=40, steering=0)
+time.sleep(2)
+jetson_control.send_command(serial_conn=port, throttle=-10, steering=100)
