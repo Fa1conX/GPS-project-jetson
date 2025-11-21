d6600103994a46bf281e4f2af94ac986ae375af8
Karsten <Fa1conX01@protonmail.com>
2025-11-20 16:15:23 -0800

    testing

--- test_script.py
+++ test_script.py
@@ -8,6 +8,7 @@ import jetson_control
 
 port = serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout=1)
 
-jetson_control.send_command(serial_conn=port, throttle=40, steering=0)
-time.sleep(2)
+jetson_control.send_command(serial_conn=port, throttle=15, steering=50)
+time.sleep(3)
 jetson_control.send_command(serial_conn=port, throttle=-10, steering=100)
+jetson_control.send_command(serial_conn=port, throttle=0, steering=90)
